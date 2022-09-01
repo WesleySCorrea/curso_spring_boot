5bac01e936987d5dd620c08c82cf59f340070138
unknown <Wesleyscorrea@hotmail.com>
2022-08-31 21:15:43 -0300

    Exception handling - delete

--- src/main/java/com/projetowesley/curso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projetowesley/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.projetowesley.curso.resources.exceptions;
 
+import com.projetowesley.curso.services.exceptions.DatabaseException;
 import com.projetowesley.curso.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,4 +20,12 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
      }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
 }
